8a5aa6bcbb21d87e2569f33979964739cb5a36e8
girim <gilmeee@naver.com>
2023-11-05 02:45:30 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 5001
+const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+
+const {User} = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://annielalacle:r14985@cluster0.apdoxpp.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindandModify:false
+mongoose.connect('config.mongoURI', {
+    useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(()=>console.log('MongoDB connected...'))
  .catch(err => console.log(err))
+
 app.get('/', (req, res)=> res.send('Hello World!'))
 
+app.post('/', async (req, res) =>{
+    //회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+    user.save()
+        .then((userInfo) => res.status(200).json({ success: true }))
+        .catch((err) => res.json({ success: false, err }));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     name: {
-        type:String,
+        type: String,
         maxlength:50
     },
     email:{
@@ -12,18 +12,18 @@ const userSchema = mongoose.Schema({
 
     },
     password:{
-        type:String,
+        type: String,
         minlength:5
     },
     lastname:{
-        type:String,
+        type: String,
         maxlength:50
     },
     role:{
         type:Number,
         default:0
     },
-    image:String,
+    image: String,
     token: {
         type:Stirng
     },
